6671d74712cd7c900972855f419ee6c4e80da149
Gabriel (Notebook) <itsgabrielmontenegro@gmail.com>
2024-04-18 17:09:29 -0300

    feat: filtros de nome e id

--- src/components/Pokemons.vue
+++ src/components/Pokemons.vue
@@ -1,17 +1,37 @@
 <template>
-    <h1>PokeCentral</h1>
+    <div>
+        <h1>PokeCentral</h1>
 
-    <ul>
-        <li v-for="pokemon in pokemons" :key="pokemon.name">
-            <PokemonCard :pokemon="pokemon" @clicked="mostraModalPokemon" />
-        </li>
-    </ul>
+        <!-- Filtros -->
+        <div>
+            <label for="">FILTRAR POR NOME</label>
+            <input
+                type="text"
+                v-model="filtroNome"
+                placeholder="Filtrar por nome"
+            />
+        </div>
+        <div>
+            <label for="">FILTRAR POR ID</label>
+            <input
+                type="number"
+                v-model.number="filtroID"
+                placeholder="Filtrar por ID"
+            />
+        </div>
 
-    <PokemonModal
-        v-if="mostrarModal"
-        :pokemon="pokemonSelecionado"
-        @close="fechaModal"
-    />
+        <ul>
+            <li v-for="pokemon in pokemonsFiltrados" :key="pokemon.name">
+                <PokemonCard :pokemon="pokemon" @clicked="mostraModalPokemon" />
+            </li>
+        </ul>
+
+        <PokemonModal
+            v-if="mostrarModal"
+            :pokemon="pokemonSelecionado"
+            @close="fechaModal"
+        />
+    </div>
 </template>
 
 <script>
@@ -29,6 +49,8 @@ export default {
             pokemons: [],
             mostrarModal: false,
             pokemonSelecionado: null,
+            filtroNome: "",
+            filtroID: null,
         };
     },
 
@@ -36,8 +58,37 @@ export default {
         axios
             .get("https://pokeapi.co/api/v2/pokemon?limit=151")
             .then((response) => {
-                this.pokemons = response.data.results;
+                this.pokemons = response.data.results.map((pokemon, index) => ({
+                    ...pokemon,
+                    id: index + 1, //+1 porque os ids da API iniciam em 1
+                }));
             });
+    },
+
+    computed: {
+        pokemonsFiltrados() {
+            let listaFiltrada = this.pokemons;
+
+            if (this.filtroNome) {
+                listaFiltrada = listaFiltrada.filter((pokemon) => {
+                    return pokemon.name
+                        .toLowerCase()
+                        .includes(this.filtroNome.toLowerCase());
+                });
+            }
+
+            if (this.filtroID !== null) {
+                listaFiltrada = listaFiltrada.filter((pokemon) => {
+                    return pokemon.id === parseInt(this.filtroID);
+                });
+            }
+
+            if (!this.filtroNome && this.filtroID === null) {
+                return this.pokemons;
+            }
+
+            return listaFiltrada;
+        },
     },
 
     methods: {
