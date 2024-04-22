ac800db30a0a5a91d6b7f5d4d321435b830b0843
Gabriel (Notebook) <itsgabrielmontenegro@gmail.com>
2024-04-22 18:52:24 -0300

    style: estiliza movimentos de ataque e jogos que o pokemon ta presente

--- src/components/PokemonGames.vue
+++ src/components/PokemonGames.vue
@@ -1,7 +1,7 @@
 <template>
-    <div>
+    <div class="jogosPresente">
         <h4>Games que este pokemon está presente</h4>
-        <ul class="lista-games">
+        <ul class="listaGames">
             <li v-for="game in games" :key="game">{{ game }}</li>
         </ul>
     </div>
@@ -15,4 +15,26 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.jogosPresente > h4 {
+    text-align: center;
+    font-size: 18px;
+    @media (min-width: 769px) {
+        font-size: 24px;
+    }
+}
+
+.listaGames {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    gap: 12px;
+
+    > li {
+        background-color: #122657;
+        color: white;
+
+        padding: 4px 12px;
+    }
+}
+</style>
--- src/components/PokemonMoves.vue
+++ src/components/PokemonMoves.vue
@@ -1,7 +1,7 @@
 <template>
-    <div>
+    <div class="movimentosDeAtaque">
         <h4>Movimentos de ataque</h4>
-        <ul class="lista-movimentos">
+        <ul class="listaMovimentos">
             <li v-for="item in moves" :key="item.move.name">
                 <span>{{ item.move.name }}</span>
             </li>
@@ -17,4 +17,29 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.movimentosDeAtaque > h4 {
+    text-align: center;
+    font-size: 18px;
+    @media (min-width: 769px) {
+        font-size: 24px;
+    }
+}
+
+.listaMovimentos {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    gap: 12px;
+
+    max-height: 130px;
+    overflow-y: scroll;
+
+    > li {
+        background-color: #122657;
+        color: white;
+
+        padding: 4px 12px;
+    }
+}
+</style>
